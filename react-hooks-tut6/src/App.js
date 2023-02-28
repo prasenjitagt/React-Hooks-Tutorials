ccab3efc55555859ce736802b1190703dfa7cd92
Prasenjit Das <businesspaymentsagt@gmail.com>
2023-02-28 11:38:06 +0530

    useEffect Hook Part 1

---
+++ react-hooks-tut6/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import EffectTut from "./componets/EffectTut";
+function App() {
+  return (
+    <div className="container">
+      useEffect
+     <EffectTut/>
+    </div>
+  );
+}
+
+export default App;
